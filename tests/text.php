6e1ced10219c5f4f4bfb5bade478575ece0f22b6
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2020-11-29 17:23:44 +0300

    Change price

---
+++ tests/text.php
@@ -0,0 +1,148 @@
+<table class="table table-dark-alt table-hover">
+        <thead>
+            <th>Порода</th>
+            <th>Цена</th>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>Акита-ину</td>
+                <td><strong> ₽</strong></td>
+            </tr>
+            <tr>
+                <td>Английский сеттер</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Афганская борзая</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Бладхаунд</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Бернский зенненхунд</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Бобтейл</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Далматин</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Доберман</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Кане-корсо</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Колли</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Лабрадор (+ золотистый ретривер)</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td> Маламут</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Немецкая овчарка</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Ньюфаундленд</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Пудель (королевский)</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Ризеншнауцер</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Ротвейлер</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Сенбернар</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Стафордширский терьер</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Тибетский мастиф</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Хаски</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Чау-чау</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Шарпей</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+            <tr>
+                <td>Эрдельтерьер</td>
+                <td><stron> ₽</stron></td>
+            </tr>
+        </tbody>
+    </table>
+
+<table class="table table-dark-alt table-hover">
+    <thead>
+        <th>Порода</th>
+        <th>Цена</th>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><strong> ₽</strong></td>
+        </tr>
+    </tbody>
+</table>
+
