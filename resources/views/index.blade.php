7e5068081859dac47470adf1faeb947f47e086a9
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2020-12-08 19:27:55 +0300

    Refactoring page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -179,7 +179,7 @@
         <div class="text-center action-price">
             <h5><strong class="color-common">МЫ ОТКРЫЛИСЬ!</strong></h5>
             <h5><strong class="color-common">АКЦИЯ!</strong></h5>
-            <h5><strong class="color-common">СКИДКУ 20% НА ПЕРВУЮ СТРИЖКУ!</strong></h5>
+            <h5><strong class="color-common">СКИДКА 20% НА ПЕРВУЮ СТРИЖКУ!</strong></h5>
         </div>
 
         <div class="text-center main-font" style="margin: 20px">
