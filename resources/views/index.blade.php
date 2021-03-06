b2258bac9b7375bbcac35991b9f64f2a3c0c8998
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2021-03-07 00:39:20 +0300

    resources/views/index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3897,8 +3897,7 @@
                     В нашем салоне доступны услуги: стрижка шерсти и коготков, купание, сушка, чистка глаз и ушей.
                 </p>
                 <ul class="list-unstyled footer-link d-flex footer-social">
-                    <li><a href="#" class="p-2"><span class="fa fa-facebook"></span></a></li>
-                    <li><a href="#" class="p-2"><span class="fa fa-instagram"></span></a></li>
+                    <li><a href="https://instagram.com/salongroom1?igshid=1pvyxcewbq2yq" class="p-2"><span class="fa fa-instagram"></span></a></li>
                 </ul>
             </div>
         </div>
