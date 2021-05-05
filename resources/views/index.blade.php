8199b7e108fdded6bd027ed3022ab4fa7a378776
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2021-05-05 18:08:16 +0300

    Resolve task

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Салон стрижки собак и кошек</title>
+    <title>Груминг салон в Москве №1 для собак и кошек</title>
     <link rel="icon" href="/public/img/favicon.ico">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.css">
