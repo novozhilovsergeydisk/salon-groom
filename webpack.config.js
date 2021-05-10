138c42faea81c8d9447aa300a08640da61d9cd2a
novozhilovsergeydisk <novozhilovsergeydisk@yandex.ru>
2021-05-10 16:56:25 +0300

    Change files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+console.log(__dirname)
+
+// module.exports = {
+//     mode: 'development',
+//     entry: path.resolve(__dirname, '/src/index.js'),
+//     output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist')
+//     }
+// }
